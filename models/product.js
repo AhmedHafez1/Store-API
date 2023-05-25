4f464cafd08b10345979ceb9c1b9118499b341ac
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2023-05-25 18:59:22 +0300

    product schema

---
+++ models/product.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+  name: { type: String, required: [true, 'name is required'] },
+  price: { type: Number, required: [true, 'price is required'] },
+  featured: { type: Boolean, default: false },
+  rating: { type: Number, default: 4.5 },
+  createdAt: { type: Date, default: Date.now() },
+  company: {
+    type: String,
+    enum: {
+      value: ['ikea', 'liddy', 'caressa', 'marcos'],
+      message: '{VALUE} is not supported',
+    },
+  },
+});
