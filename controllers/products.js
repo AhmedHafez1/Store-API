0b172ceaa2555901362d207fbbef2ff31c7ac0c5
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2023-05-26 07:19:55 +0300

    refactor query object

--- controllers/products.js
+++ controllers/products.js
@@ -1,6 +1,17 @@
 const Product = require('../models/product');
 const getAllProducts = async (req, res) => {
-  const products = await Product.find(req.query);
+  const { featured } = req.query;
+
+  const queryObject = {};
+
+  if (featured) {
+    queryObject.featured = featured === 'true';
+  }
+
+  console.log(req.query);
+  console.log(queryObject);
+
+  const products = await Product.find(queryObject);
   res.status(200).json({ data: products, count: products.length });
 };
 
