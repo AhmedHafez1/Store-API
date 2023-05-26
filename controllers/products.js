b7bc1cfeff049a194c5dacdf32e151aebdf8b796
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2023-05-26 07:26:50 +0300

    filter by company

--- controllers/products.js
+++ controllers/products.js
@@ -1,15 +1,15 @@
 const Product = require('../models/product');
 const getAllProducts = async (req, res) => {
-  const { featured } = req.query;
+  const { featured, company } = req.query;
 
   const queryObject = {};
 
   if (featured) {
     queryObject.featured = featured === 'true';
   }
-
-  console.log(req.query);
-  console.log(queryObject);
+  if (company) {
+    queryObject.company = company;
+  }
 
   const products = await Product.find(queryObject);
   res.status(200).json({ data: products, count: products.length });
